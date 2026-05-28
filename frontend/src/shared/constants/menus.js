17ac86ac5f5858d3e48af96ec723b023b2acd875
alejo1781 <leon_orrego_@hotmail.com>
2026-05-28 14:43:19 -0500

    change(shared): se cambia la palabra usuario por recepcionista para que sea mas explicita la funcion que hace

--- frontend/src/shared/constants/menus.js
+++ frontend/src/shared/constants/menus.js
@@ -4,7 +4,7 @@ export const OWNER_MENU = [
   { label: "Dashboard", href: ROUTES.OWNER_HOME },
   { label: "Consultar facturación", href: ROUTES.OWNER_BILLING },
   { label: "Gestionar contrato", href: ROUTES.OWNER_CONTRACTS },
-  { label: "Crear nuevo usuario", href: ROUTES.OWNER_USERS },
+  { label: "Crear nuevo recepcionista", href: ROUTES.OWNER_USERS },
 ];
 
 // Más adelante podrás agregar:
