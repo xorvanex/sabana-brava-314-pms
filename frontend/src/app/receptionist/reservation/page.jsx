4f9750e4294591d9649f607e52b7049087e5cf57
alejo1781 <leon_orrego_@hotmail.com>
2026-06-05 11:04:58 -0500

    add(receptionist): se añade la invocacion de la vista de reservacion

---
+++ frontend/src/app/receptionist/reservation/page.jsx
@@ -0,0 +1,6 @@
+// src/app/receptionist/reservation/page.jsx
+import RegisterReservationView from "@/receptionist/components/reservation/RegisterReservationView";
+
+export default function ReservationPage() {
+  return <RegisterReservationView />;
+}
