ad712d2f183783fda206811435a623bec0d7dcb4
alejo1781 <leon_orrego_@hotmail.com>
2026-06-07 20:00:25 -0500

    Update page.jsx

--- frontend/src/app/receptionist/reservation/page.jsx
+++ frontend/src/app/receptionist/reservation/page.jsx
@@ -1,4 +1,4 @@
-// src/app/receptionist/reservation/page.jsx
+
 import RegisterReservationView from "@/receptionist/components/reservation/RegisterReservationView";
 
 export default function ReservationPage() {
