c67e31d13e499252db6a049995bde34db0e1d70d
Dago Palmera <dagopalmera1@gmail.com>
2026-06-20 16:35:06 -0500

    refactor(main): Se agrego restriccion de dominio al middleware CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="Hotel Sabana Brava 314")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
